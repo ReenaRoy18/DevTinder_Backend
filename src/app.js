6468b5f092d0550b210ee32fcf2644b75bd37957
Reena <Reenaroy@deducetech.com>
2025-02-11 12:24:09 +0530

    implemented rate-limit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const {authRouter} = require("./routes/auth.js")
 const {profileRouter} = require("./routes/profile.js")
 const {requestRouter} = require("./routes/request.js")
 const {userRouter} = require("./routes/user.js")
+const {rateLimit} = require("express-rate-limit") //rate-limit
 let cors = require('cors')
 require("dotenv").config();
 
@@ -16,15 +17,25 @@ let corsOptions = {
     credentials:true
   }
 
+const limiter = rateLimit({  //rate-limit
+    windowMs:60*1000, //how much min 
+    max:5, //how many requests need to limit
+    message:"Too many requests not allowed", //if too many requests show err message
+    headers:true
+})
+
+
+
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors(corsOptions))
 
+app.use(limiter) //It is used to limit the repeated requests
+app.use("/api", authRouter);
+app.use("/api", profileRouter);
+app.use("/api", requestRouter);
+app.use("/api", userRouter)
 
-app.use("/", authRouter);
-app.use("/", profileRouter);
-app.use("/", requestRouter);
-app.use("/", userRouter)
 
 
 
